28e50b91b1dd3df492e93cb1d3481936dbb0fbb4
Tommaso Figlia <tommasofiglia@gmail.com>
2021-02-10 16:00:41 +0100

    Effettuato il collegamento di due views

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>about us</title>
+  </head>
+  <body>
+    <a href="/">vai ad Home</a>
+  </body>
+</html>
